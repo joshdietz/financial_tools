8976f35c130d224bfa5ec228e688ec84b9f50778
Josh Dietz <joshdietz@outlook.com>
2022-09-04 19:48:30 -0700

    adding some helper methods

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
+import sqlite3
+import psycopg2
 import dotenv
 
 dotenv.load_dotenv()
@@ -12,6 +14,21 @@ bcrypt = Bcrypt(app)
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'page': 'index page'})
+
+"""
+    *** Helper Methods ***
+"""
+def hash_password(password):
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
+def check_password(password, hashed_password):
+    return bcrypt.check_password_hash(hashed_password, password)
+
+def connect_db():
+    if os.getenv('DB_DRIVER') == 'sqlite':
+        return sqlite3.connect(os.getenv('DB_NAME'))
+    elif os.getenv('DB_DRIVER') == 'postgres':
+        return psycopg2.connect(os.getenv('DB_NAME'))
 
 if __name__ == '__main__':
     app.run(debug=True)
